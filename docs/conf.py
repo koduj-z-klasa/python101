0df55be27c1e3c4cffef5f12ac8ddbc9b440547c
Janusz Skonieczny <js@bravelabs.pl>
2014-09-16 20:33:20 +0200

    do not require sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -262,6 +262,10 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+try:
+    import sphinx_rtd_theme
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+except:
+    pass
