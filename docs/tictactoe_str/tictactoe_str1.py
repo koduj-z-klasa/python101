471114f0d20ee797a9deb93d22d973849c89d6cf
xinulsw <xinulsw@gmail.com>
2014-11-18 18:16:20 +0100

    Plik tictactoe_str1.py

---
+++ docs/tictactoe_str/tictactoe_str1.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pygame, sys, random
+from pygame.locals import * #udostępnienie nazw metod z locals
+
+# inicjacja modułu pygame
+pygame.init()
+
+# przygotowanie powierzchni do rysowania, czyli inicjacja okna gry
+OKNOGRY = pygame.display.set_mode((150, 150), 0, 32)
+# tytuł okna gry
+pygame.display.set_caption('Kółko i krzyżyk')
+
+# lista opisująca stan pola gry, 0 - pole puste, 1 - gracz, 2 - komputer
+POLE_GRY = [0,0,0,
+            0,0,0,
+            0,0,0]
+
+RUCH = 1 # do kogo należy ruch: 1 – gracz, 2 – komputer
+WYGRANY = 0 # wynik gry: 0 - nikt, 1 - gracz, 2 - komputer, 3 - remis
+WYGRANA = False
