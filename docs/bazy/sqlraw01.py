8a41b03e66f47a9c25f7767e744e275c0e081a6a
xinulsw <xinulsw@gmail.com>
2014-12-06 20:27:54 +0100

    Dodanie pliku sqlraw01.py

---
+++ docs/bazy/sqlraw01.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import sqlite3
+
+# utworzenie połączenia z bazą przechowywaną na dysku
+# lub w pamięci (':memory:')
+con = sqlite3.connect('test.db')
+
+# dostęp do kolumn przez indeksy i przez nazwy
+con.row_factory = sqlite3.Row
+
+# utworzenie obiektu kursora
+cur = con.cursor()
