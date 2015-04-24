2a74c77d225be89a88de73a3d164e6d41ff4b343
xinulsw <xinulsw@gmail.com>
2015-04-24 11:49:38 +0200

    Poprawki w scenariuszu Quiz.

--- docs/webapps/quiz/quiz3.py
+++ docs/webapps/quiz/quiz3.py
@@ -1,42 +1,16 @@
 # -*- coding: utf-8 -*-
+# quiz/quiz3.py
 
-# niezbędne importy
 from flask import Flask
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template
 
 app = Flask(__name__)
 
-# konfiguracja aplikacji
-app.config.update(dict(
-    # nieznany nikomu sekret
-    SECRET_KEY='bradzosekretnawartosc',
-))
-
-# lista pytań
-PYTANIA = [
-    {
-        'pytanie': u'Stolica Hiszpani, to:',# pytanie
-        'odpowiedzi': [u'Madryt', u'Warszawa', u'Barcelona'], # możliwe odpowiedzi
-        'odpok': u'Madryt', # poprawna odpowiedź
-    },
-    {
-        'pytanie': u'Objętość sześcianu o boku 6 cm, wynosi:',
-        'odpowiedzi': [u'36', u'216', u'18'],
-        'odpok': u'216',
-    },
-    {
-        'pytanie': u'Symbol pierwiastka Helu, to:',
-        'odpowiedzi': [u'Fe', u'H', u'He'],
-        'odpok': u'He',
-    }
-]
-
-
+# dekorator łączący adres główny z widokiem index
 @app.route('/')
 def index():
-    # jeżeli zadanie jest typu GET, to renderujemy szablon index.html,
-    # do którego przekazujemy tablicę pytań w zmiennej "pytania"
-    return render_template('index.html', pytania=PYTANIA)
+    #return 'Cześć, tu Python!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
