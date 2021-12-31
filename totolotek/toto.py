5d5e470935f6b8e5f0f5923ffe8bbe330dc10f9c
xinulsw <xinulsw@gmail.com>
2021-12-31 11:45:14 +0100

    składnia python2 na python3

--- totolotek/toto.py
+++ totolotek/toto.py
@@ -7,16 +7,16 @@ liczba = random.randint(1, 10)
 # print "Wylosowana liczba:", liczba
 
 for i in range(3):
-    print "Próba ", i+1
-    odp = raw_input("Jaką liczbę od 1 do 10 mam na myśli? ")
+    print("Próba ", i + 1)
+    odp = input("Jaką liczbę od 1 do 10 mam na myśli? ")
     # print "Podałeś liczbę: ", odp
 
     if liczba == int(odp):
-        print "Zgadłeś! Dostajesz długopis!"
+        print("Zgadłeś! Dostajesz długopis!")
         break
     else:
         if i == 2:
-            print "Miałem na myśli liczbę: ", liczba
+            print("Miałem na myśli liczbę: ", liczba)
         else:
-            print "Nie zgadłeś. Spróbuj jeszcze raz."
+            print("Nie zgadłeś. Spróbuj jeszcze raz.")
         print
